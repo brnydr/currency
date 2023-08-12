db3730c91d0f69f0a0aaf6ae6d5a67099fc8f65a
brnydr <brianwilliamyoder@gmail.com>
2023-08-12 12:04:52 -0700

    refactor code for error handling

--- src/services/currencyService.js
+++ src/services/currencyService.js
@@ -1,15 +1,7 @@
 export default class CurencyService {
-  static async getRates(countryCode) {
-    try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${countryCode}`);
-      if (!response.ok) {
-        let errorMessage = `${response.status} (${response.statusText})`;
-        throw Error(errorMessage);
-      }
+  static async getRates(countryCode1, countryCode2, amount) {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${countryCode1}/${countryCode2}/${amount}`);
       return response.json();
     }
-    catch(error){
-      return error;
-    }
   }
-}
+
