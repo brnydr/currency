6ac43678cdd09b694ac96f47dc93cfacabfbe736
brnydr <brianwilliamyoder@gmail.com>
2023-08-12 12:08:18 -0700

    delete whitespace

--- src/services/currencyService.js
+++ src/services/currencyService.js
@@ -1,7 +1,7 @@
 export default class CurencyService {
   static async getRates(countryCode1, countryCode2, amount) {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${countryCode1}/${countryCode2}/${amount}`);
-      return response.json();
-    }
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${countryCode1}/${countryCode2}/${amount}`);
+    return response.json();
   }
+}
 
